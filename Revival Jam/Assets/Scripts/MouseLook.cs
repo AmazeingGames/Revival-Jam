d523b3925458f8cec9c71664705ebdfb253e49df
Amazeing Games <AmazeingGameDesign@gmail.com>
2023-10-10 22:26:26 -0700

    Player Movement + Files
    Initial commit with all Unity files, along with barebones scenes and scripts.

---
+++ Revival Jam/Assets/Scripts/MouseLook.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public enum RotationAxes { MouseXY, MouseX, MouseY }
+
+    public RotationAxes axes = RotationAxes.MouseXY;
+
+    public float sensitvityVer = 9f;
+    public float sensitivityHor = 9f;
+
+    public float minimumVert = -45f;
+    public float maximumVert = 45f;
+
+    float verticalRot = 0;
+
+    void CalcVertRot()
+    {
+        verticalRot -= Input.GetAxis("Mouse Y") * sensitvityVer;
+        verticalRot = Mathf.Clamp(verticalRot, minimumVert, maximumVert);
+    }
+
+    float GetXRot()
+    {
+        return Input.GetAxis("Mouse X") * sensitivityHor;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rigidbody body = GetComponent<Rigidbody>();
+        if(body != null)
+        {
+            body.freezeRotation = true;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (axes == RotationAxes.MouseX)
+        {
+            transform.Rotate(0, GetXRot(), 0);
+        }
+        else if (axes == RotationAxes.MouseY)
+        {
+            CalcVertRot();
+
+            float horizontalRot = transform.localEulerAngles.y;
+
+            transform.localEulerAngles = new Vector3(verticalRot, horizontalRot, 0);
+        }
+        else
+        {
+            CalcVertRot();
+
+            float delta = GetXRot();
+            float horizontalRot = transform.localEulerAngles.y + delta;
+
+            transform.localEulerAngles = new Vector3(verticalRot, horizontalRot, 0);
+        }
+    }
+}
