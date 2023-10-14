e07c6c3fbfd46dc30450d6b137f50bb6cb391238
Amazeing Games <AmazeingGameDesign@gmail.com>
2023-10-14 12:01:47 -0700

    Organized Files
    Organized project file structure.

---
+++ Revival Jam/Assets/_Scripts/Managers/MouseManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        #if DEBUG
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                Cursor.visible = !Cursor.visible;
+                Cursor.lockState = Cursor.visible ? CursorLockMode.None : CursorLockMode.Locked;
+            }
+        #endif
+    }
+}
