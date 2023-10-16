6d39e5cf301565ca3f0f90806d059fe624b9955c
Amazeing Games <AmazeingGameDesign@gmail.com>
2023-10-16 07:30:47 -0700

    Minor Changes to Wire
    Adding OnClickEvents and button component. Needed for testing.

--- Revival Jam/Assets/Prefabs/Wire.cs
+++ Revival Jam/Assets/Prefabs/Wire.cs
@@ -1,9 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
-public class Wire : MonoBehaviour
+public class Wire : MonoBehaviour, IPointerDownHandler, IPointerMoveHandler
 {
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log("Clicked");
+    }
+
+    public void OnPointerMove(PointerEventData eventData)
+    {
+        Debug.Log("Move");
+    }
+
     // Start is called before the first frame update
     void Start()
     {
