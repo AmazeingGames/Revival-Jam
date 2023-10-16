81f7caa97daa9bc7e5fa2710fffa7f9ce6e1343f
Amazeing Games <AmazeingGameDesign@gmail.com>
2023-10-16 11:12:21 -0700

    Figuring out Collsions
    Can't seem to get any kind of colliders to interact with eachother in any meaningful way. Will most likely try raycasting next.

--- Revival Jam/Assets/Prefabs/Wire.cs
+++ Revival Jam/Assets/Prefabs/Wire.cs
@@ -4,22 +4,32 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 using UnityEngine.UIElements;
+using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class Wire : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
 {
     [SerializeField] float sensitivity;
+    [SerializeField] string receptacleTag;
 
     bool shouldFollowMouse = false;
 
-    Vector3 startingMousePosition;
     private Vector2? lastMousePoint = null;
+
+
+    ReceptacleObject connectedReceptacle;
+
+    List<Collider> overlappingColliders;
+
+    void Start()
+    {
+    }
 
     public void OnPointerDown(PointerEventData eventData)
     {
         Debug.Log("Clicked");
 
         shouldFollowMouse = true;
-        
+
         lastMousePoint = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
 
     }
@@ -27,12 +37,8 @@ public class Wire : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
     public void OnPointerUp(PointerEventData eventData)
     {
         shouldFollowMouse = false;
-    }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+        CheckCurrentCollisions();
     }
 
     // Update is called once per frame
@@ -46,15 +52,29 @@ public class Wire : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
         if (!shouldFollowMouse)
             return;
 
-            float differenceX = Input.mousePosition.x - lastMousePoint.Value.x;
+        float differenceX = Input.mousePosition.x - lastMousePoint.Value.x;
 
-            float differenceY = Input.mousePosition.y - lastMousePoint.Value.y;
+        float differenceY = Input.mousePosition.y - lastMousePoint.Value.y;
 
-            transform.position = new Vector3(transform.position.x + (differenceX / 188) * Time.deltaTime * sensitivity, transform.position.y + (differenceY / 188) * Time.deltaTime * sensitivity, transform.position.z);
+        transform.position = new Vector3(transform.position.x + (differenceX / 188) * Time.deltaTime * sensitivity, transform.position.y + (differenceY / 188) * Time.deltaTime * sensitivity, transform.position.z);
 
-            lastMousePoint = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
-
-
+        lastMousePoint = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
     }
 
+    void CheckCurrentCollisions()
+    {
+        List<Collider2D> overlappingColliders = new();
+
+        for (int i = 0; i < overlappingColliders.Count; i++)
+        {
+            var collider = overlappingColliders[i];
+
+            Debug.Log($"Loop : {collider.gameObject.name}");
+
+            if (!collider.gameObject.CompareTag($"{receptacleTag}"))
+                continue;
+
+            collider.gameObject.SetActive(false);
+        }
+    }
 }
