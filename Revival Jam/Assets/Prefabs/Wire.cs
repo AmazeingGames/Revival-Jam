00910031b18ddbe97bcd9ff2fcf5f3f624210a90
Amazeing Games <AmazeingGameDesign@gmail.com>
2023-10-16 09:51:25 -0700

    Movable Wires
    Wires can be successfully dragged and dropped around the panel.

--- Revival Jam/Assets/Prefabs/Wire.cs
+++ Revival Jam/Assets/Prefabs/Wire.cs
@@ -3,17 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
+using UnityEngine.UIElements;
 
-public class Wire : MonoBehaviour, IPointerDownHandler, IPointerMoveHandler
+public class Wire : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
 {
+    [SerializeField] float sensitivity;
+
+    bool shouldFollowMouse = false;
+
+    Vector3 startingMousePosition;
+    private Vector2? lastMousePoint = null;
+
     public void OnPointerDown(PointerEventData eventData)
     {
         Debug.Log("Clicked");
+
+        shouldFollowMouse = true;
+        
+        lastMousePoint = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+
     }
 
-    public void OnPointerMove(PointerEventData eventData)
+    public void OnPointerUp(PointerEventData eventData)
     {
-        Debug.Log("Move");
+        shouldFollowMouse = false;
     }
 
     // Start is called before the first frame update
@@ -25,6 +38,23 @@ public class Wire : MonoBehaviour, IPointerDownHandler, IPointerMoveHandler
     // Update is called once per frame
     void Update()
     {
-        
+        FollowMouse();
     }
+
+    void FollowMouse()
+    {
+        if (!shouldFollowMouse)
+            return;
+
+            float differenceX = Input.mousePosition.x - lastMousePoint.Value.x;
+
+            float differenceY = Input.mousePosition.y - lastMousePoint.Value.y;
+
+            transform.position = new Vector3(transform.position.x + (differenceX / 188) * Time.deltaTime * sensitivity, transform.position.y + (differenceY / 188) * Time.deltaTime * sensitivity, transform.position.z);
+
+            lastMousePoint = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+
+
+    }
+
 }
--- Revival Jam/Assets/VirtualScreen.cs
+++ Revival Jam/Assets/VirtualScreen.cs
@@ -9,11 +9,13 @@ public class VirtualScreen : GraphicRaycaster
 
     public GraphicRaycaster screenCaster; // Reference to the GraphicRaycaster of the canvas displayed on the virtual screen
 
+
     // Called by Unity when a Raycaster should raycast because it extends BaseRaycaster.
     public override void Raycast(PointerEventData eventData, List<RaycastResult> resultAppendList)
     {
         Ray ray = eventCamera.ScreenPointToRay(eventData.position); // Mouse
         RaycastHit hit;
+
         if (Physics.Raycast(ray, out hit))
         {
             // Figure out where the pointer would be in the second camera based on texture position or RenderTexture.
