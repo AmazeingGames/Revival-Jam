c154510521c82729f1dad876e8cae765219b44f9
Amazeing Games <AmazeingGameDesign@gmail.com>
2023-10-27 17:52:21 -0700

    Fixed a Few things on Level 1

--- Revival Jam/Assets/_Scripts/Systems/StartActive.cs
+++ Revival Jam/Assets/_Scripts/Systems/StartActive.cs
@@ -11,10 +11,4 @@ public class StartActive : MonoBehaviour
     {
         gameObject.SetActive(startActive);
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
