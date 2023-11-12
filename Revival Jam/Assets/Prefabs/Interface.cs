ce23c102ffd33d72ba412a82da3e5f92ef37228a
Amazeing Games <AmazeingGameDesign@gmail.com>
2023-11-11 18:58:57 -0800

    Interface Base Functionality
    Fixed The Interface so it works properly. Player can interact with it using a tool to trigger a line of code. Currently no further functionality hooked up.

--- Revival Jam/Assets/Prefabs/Interface.cs
+++ Revival Jam/Assets/Prefabs/Interface.cs
@@ -7,9 +7,9 @@ public class Interface : MonoBehaviour
 {
     [SerializeField] InterfaceData InterfaceData;
 
-    private void OnMouseDown()
+    private void OnMouseUp()
     {
-        Debug.Log("Mouse click!");
+        Debug.Log("Mouse up");
 
         UseItemOnInterface();
     }
@@ -17,14 +17,24 @@ public class Interface : MonoBehaviour
 
     void UseItemOnInterface()
     {
-        if (HotbarManager.Instance.HoldingItem.ItemType != InterfaceData.InterfaceTool)
+        var holdingItem = HotbarManager.Instance.HoldingItem;
+        if (holdingItem == null || holdingItem.ItemType != InterfaceData.InterfaceTool)
+        {
+            if (holdingItem != null)
+            {
+                Debug.Log($"holding wrong item | holding : {holdingItem.ItemType} - Required Item : {InterfaceData.InterfaceTool}");
+            }
+            else
+                Debug.Log($"Not holding an item | holdingItem is null {holdingItem == null}");
             return;
+
+        }
 
         Debug.Log("Passed check 1");
 
         if (PlayerFocus.Instance.Focused != InterfaceData.InterfaceType)
             return;
 
-        Debug.Log("Used Item on Interface");
+        Debug.Log($"Used Item on Interface {InterfaceData.name}");
     }
 }
