2b1985f26bf4e3107ee1fbd95b99fdc0c3b49620
Amazeing Games <AmazeingGameDesign@gmail.com>
2023-12-12 19:58:24 -0800

    Fixed Button Jammed
    Added visuals that change for the button when using the wrench, handled by the arcade animator

--- Revival Jam/Assets/_Scripts/Managers/CabinetAnimator.cs
+++ Revival Jam/Assets/_Scripts/Managers/CabinetAnimator.cs
@@ -17,6 +17,19 @@ public class CabinetAnimator : MonoBehaviour
     float target;
     float horizontalInput;
 
+    [Header("Jammed Button")]
+    [SerializeField] Transform button;
+
+    private void OnEnable()
+    {
+        Interface.UseItem += HandleUseItem;   
+    }
+
+    private void OnDisable()
+    {
+        Interface.UseItem -= HandleUseItem;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -39,14 +52,28 @@ public class CabinetAnimator : MonoBehaviour
         if (PlayerFocus.Instance != null && PlayerFocus.Instance.Focused != PlayerFocus.FocusedOn.Arcade)
             horizontalInput = 0;
 
-
-
-            target = .5f * (horizontalInput + 1);
+        target = .5f * (horizontalInput + 1);
 
         current = Mathf.MoveTowards(current, target, speed * Time.deltaTime);
         
         joystick.localRotation = Quaternion.Lerp(Quaternion.Euler(minRotation), Quaternion.Euler(maxRotation), curve.Evaluate(current));
     }
 
+    void HandleUseItem(ItemData itemData)
+    {
+        switch (itemData.ItemType)
+        {
+            case ItemAndAbilityManager.ItemsAndAbilities.Screwdriver:
+            case ItemAndAbilityManager.ItemsAndAbilities.Wrench:
+                FixButton();
+                break;
+        }
+    }
+
+    //When the player fixes the jammed button with the tool, returns it to its normal visuals
+    void FixButton()
+    {
+        button.localRotation = Quaternion.Euler(Vector3.zero);
+    }
 
 }
