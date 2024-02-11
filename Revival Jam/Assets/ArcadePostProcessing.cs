eac259aa91c415abe0829e0ea7211bf9a3639755
Amazeing Games <AmazeingGameDesign@gmail.com>
2024-02-10 20:33:54 -0800

    Added Shaders to Each Level
    Added custom shaders that scale up in intensity for each level in the game.

---
+++ Revival Jam/Assets/ArcadePostProcessing.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class ArcadePostProcessing : MonoBehaviour
+{
+    [SerializeField] Volume TerminalPostProcessing;
+    [SerializeField] Volume LevelPostProcessing;
+
+
+    private void OnEnable()
+    {
+        DialogueManager.EnterDialogue += HandleEnterDialogue;
+    }
+
+    private void OnDisable()
+    {
+        DialogueManager.EnterDialogue -= HandleEnterDialogue;
+    }
+
+    private void Update()
+    {
+        //Starts level post on level start
+        if (!TerminalPostProcessing.isActiveAndEnabled)
+            LevelPostProcessing.gameObject.SetActive(true);
+    }
+
+    //true -> open dialogue
+    //false -> close dialogue
+    //Responsible for deciding which post processing to use, since we want to use different processes for the terminal and the level
+    void HandleEnterDialogue(bool dialogueOpen)
+    {
+        TerminalPostProcessing.gameObject.SetActive(dialogueOpen);
+        LevelPostProcessing.gameObject.SetActive(!dialogueOpen);
+    }
+}
