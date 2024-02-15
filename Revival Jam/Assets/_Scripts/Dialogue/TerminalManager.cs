7e30d9fc2a653178380b60f980408542f50854a9
Amazeing Games <AmazeingGameDesign@gmail.com>
2024-02-14 20:04:40 -0800

    Added Terminal Interpretation
    Created a base and functional terminal interpreter that can respond to the player's input. To Do: Refactor to be scalable, use the dialogue manager and dialogue objects, and more closely follow the command pattern

--- Revival Jam/Assets/_Scripts/Dialogue/TerminalManager.cs
+++ Revival Jam/Assets/_Scripts/Dialogue/TerminalManager.cs
@@ -16,6 +16,11 @@ public class TerminalManager : Singleton<TerminalManager>
     [SerializeField] ScrollRect scrollRect;
     [SerializeField] RectTransform commandLineContainer;
 
+    [Header("Terminal")]
+    [SerializeField] Interpreter interpreter;
+    [SerializeField] float scrollAmount;
+
+    List<string> previousInput;
 
     private void OnGUI()
     {
@@ -30,15 +35,45 @@ public class TerminalManager : Singleton<TerminalManager>
             commandLineContainer.sizeDelta = new Vector2(commandLineContainerSize.x, commandLineContainerSize.y + 35f);
 
             GameObject message = Instantiate(directoryLine, commandLineContainer);
-
             message.transform.SetSiblingIndex(commandLineContainer.transform.childCount - 1);
-
             message.GetComponentInChildren<TMP_Text>().text = userInput;
+
+            int lines = DisplayInterpretation(interpreter.Interpret(userInput));
+            ScrollToBottom(lines);
 
             userInputLine.transform.SetAsLastSibling();
 
             terminalInput.ActivateInputField();
             terminalInput.Select();
+        }
+    }
+
+    int DisplayInterpretation(Interpreter.Command interpretedCommand)
+    {
+        for (int i = 0; i < interpretedCommand.GetResponse().Count; i++)
+        {
+            GameObject response = Instantiate(responseLine, commandLineContainer);
+
+            response.transform.SetAsLastSibling();
+
+            Vector2 listSize = commandLineContainer.sizeDelta;
+            commandLineContainer.sizeDelta = new Vector2(listSize.x, listSize.y + 35);
+
+            response.GetComponentInChildren<TMP_Text>().text = interpretedCommand.GetResponse()[i];
+        }
+        
+        return interpretedCommand.GetResponse().Count;
+    }
+
+    void ScrollToBottom(int lines)
+    {
+        if (lines > 4)
+        {
+            scrollRect.velocity = new Vector2(0, 450);
+        }
+        else
+        {
+            scrollRect.verticalNormalizedPosition = 0 + scrollAmount;
         }
     }
 }
---
+++ Revival Jam/Assets/_Scripts/Managers/Interpreter.cs
@@ -0,0 +1,134 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Interpreter : MonoBehaviour
+{
+    List<string> response = new();
+
+    readonly HelpCommand help = new("help");
+    readonly NoteCommand note = new("note");
+    readonly UnknownCommand unknown = new();
+
+    List<Command> commands = new();
+
+    private void Start()
+    {
+        commands = new List<Command>()
+        {
+            help,
+            note,
+        };
+    }
+
+    //Finds and executes the matching command
+    public Command Interpret(string userInput)
+    {
+        response.Clear();
+
+        userInput = userInput.Replace(" ", "");
+
+        for (int i = 0; i < commands.Count; i++)
+        {
+            var command = commands[i];
+
+            if (DoesKeyMatch(userInput, command))
+            {
+                return command;
+            }
+        }
+        return unknown;
+    }
+
+    class UnknownCommand : Command
+    {
+        public override void Execute()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override List<string> GetResponse()
+        {
+            var response = new List<string>
+            {
+                "I have no clue what you are saying, like literally no clue.",
+                "Get some help. Seriously: 'help'."
+            };
+
+            return response;
+        }
+    }
+
+    class HelpCommand : Command
+    {
+        public HelpCommand(params string[] commandKeys) : base (commandKeys) { }
+
+        override public void Execute()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override List<string> GetResponse()
+        {
+            var response = new List<string>
+            {
+                "If you need help, you should see a doctor.",
+                "Alternatively, a therapist can work wonders."
+            };
+
+            return response;
+        }
+    }
+
+    class NoteCommand : Command
+    {
+        public NoteCommand(params string[] commandKeys) : base(commandKeys) { }
+
+        override public void Execute() 
+        {
+            throw new System.NotImplementedException(); 
+        }
+
+        public override List<string> GetResponse()
+        {
+            var response = new List<string>
+            {
+                "This is the first line of the note.",
+                "This is the second line of the note."
+            };
+
+            return response;
+        }
+    }
+
+    public abstract class Command
+    {
+        public IList<string> CommandKeys { get => commandKeys.AsReadOnlyList(); }
+        readonly List<string> commandKeys;
+
+        public Command(params string[] commandKeys) 
+        {
+            this.commandKeys = commandKeys.ToList();    
+        }
+
+        public abstract void Execute();
+
+        public abstract List<string> GetResponse();
+    }
+
+    bool DoesKeyMatch(string key, Command command)
+    {
+        string keyLower = key.ToLower();
+
+        for (int i = 0; i < command.CommandKeys.Count; i++)
+        {
+            var currentLower = command.CommandKeys[i].ToLower();
+
+            if (currentLower == keyLower)
+                return true;
+        }
+        return false;
+    }
+}
